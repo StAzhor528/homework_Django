014ba9d8bffe67112cf9d00a8385e190dfa1f852
Ivan <stazhor@bk.ru>
2022-05-20 15:59:16 +0300

    order_jQuery, django-dynamic-formset

---
+++ geekshop/geekshop/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from mainapp.views import get_products
+
+
+def index(request):
+    products = get_products(3)
+    # basket = get_basket(request.user)
+
+    context = {
+        'title': 'главная',
+        'products': products,
+        # 'basket': basket,
+    }
+
+    return render(request, 'geekshop/index.html', context)
+
+
+def contacts(request):
+    # basket = get_basket(request.user)
+    context = {
+        'title': 'контакты',
+        # 'basket': basket,
+    }
+    return render(request, 'geekshop/contact.html', context)
